7ae7b4f6bef129f98aebc981763ac3ce9df9fefc
NindyaShafira24 <nindyashafira585@gmail.com>
2025-09-24 19:02:19 +0700

    first commit

---
+++ bin/bmi_kalkulator_3.dart
@@ -0,0 +1,5 @@
+import 'package:bmi_kalkulator_3/bmi_kalkulator_3.dart' as bmi_kalkulator_3;
+
+void main(List<String> arguments) {
+  print('Hello world: ${bmi_kalkulator_3.calculate()}!');
+}
---
+++ test/bmi_kalkulator_3_test.dart
@@ -0,0 +1,8 @@
+import 'package:bmi_kalkulator_3/bmi_kalkulator_3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
